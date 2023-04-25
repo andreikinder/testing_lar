6cd233d2e85651996f125ab04e6d87c2efa0703d
andre_gthv1mg <anrei.kinder.170@gmail.com>
2023-04-25 08:58:10 +0500

    product Unit Test

---
+++ testing_lar/tests/Unit/ProductTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Tests\Unit;
+use App\Product;
+use PHPUnit\Framework\TestCase;
+
+
+class ProductTest extends  TestCase
+{
+
+
+    /**
+     * @var Product
+     */
+    protected $product;
+
+    protected function setUp() : void
+    {
+      $this->product = new Product('Fallout 4', 59);
+    }
+
+    /** @test   */
+    public function a_product_has_a_name()
+    {
+        $this->assertEquals('Fallout 4', $this->product->name());
+    }
+
+    /** @test   */
+    public function a_product_has_a_cost()
+    {
+        $this->assertEquals( 59,  $this->product->cost());
+    }
+}
